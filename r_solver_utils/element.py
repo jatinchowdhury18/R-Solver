d2d408fe1bf63c8e256775be29e9cc4d3b35400a
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-01-19 13:30:02 +0000

    Minor changes to make sure the script is compatible with earlier version of Python

--- r_solver_utils/element.py
+++ r_solver_utils/element.py
@@ -1,7 +1,17 @@
-from collections import namedtuple
+from collections import namedtuple, Mapping
+
+def namedtuple_with_defaults(typename, field_names, default_values=()):
+    T = namedtuple(typename, field_names)
+    T.__new__.__defaults__ = (None,) * len(T._fields)
+    if isinstance(default_values, Mapping):
+        prototype = T(**default_values)
+    else:
+        prototype = T(*default_values)
+    T.__new__.__defaults__ = tuple(prototype)
+    return T
 
 fields = ('type', 'node1', 'node2', 'impedance', 'port', 'node3', 'node4', 'gain')
-Element = namedtuple('Element', fields, defaults=(None,) * len(fields))
+Element = namedtuple_with_defaults('Element', fields, (None,) * len(fields))
 
 RES_TYPE = 'Res'
 VOLTAGE_TYPE = 'Vs'
--- r_solver_utils/print_helpers.py
+++ r_solver_utils/print_helpers.py
@@ -7,16 +7,16 @@ def print_matrix(M, out_file, num_ports, adapt_expr, args, custom_args):
 
     if custom_args:
         args_str = 'r_solver.py '
-        args_str += f'--datum {args.datum} '
-        args_str += f'--adapt {args.adapted_port} '
-        args_str += f'--verbose {args.verbose} '
-        args_str += f'--out {args.out_file.name} '
-        args_str += f'{args.netlist.name}'
+        args_str += '--datum ' + str(args.datum) + ' '
+        args_str += '--adapt ' + str(args.adapted_port) + ' '
+        args_str += '--verbose ' + str(args.verbose) + ' '
+        args_str += '--out ' + args.out_file.name + ' '
+        args_str += args.netlist.name
         comments += '// invoked with command: ' + args_str + '\n'
     else:
         args = subprocess.list2cmdline(sys.argv[:])
         comments += '// invoked with command: ' + args + '\n'
-    prefix = f'const auto S_matrix[{num_ports}][{num_ports}] = {{'
+    prefix = 'const auto S_matrix[' + str(num_ports) + '][' + str(num_ports) + '] = {{'
     empty_prefix = ' ' * len(prefix)
     
     M_strs = M.str(rep_mapping=lambda a: str(a) + ',')
@@ -30,12 +30,12 @@ def print_matrix(M, out_file, num_ports, adapt_expr, args, custom_args):
     for f in re.finditer(re.escape('^2'), M_strs):
         s_ind = f.start() + count
         chars_to_square = M_strs[s_ind-2:s_ind]
-        M_strs = M_strs[:s_ind] + f'*{chars_to_square}' + M_strs[s_ind+2:]
+        M_strs = M_strs[:s_ind] + '*' + str(chars_to_square) + M_strs[s_ind+2:]
         count += 1
 
     if out_file is not None:
         if adapt_expr is not None:
-            M_strs += f'\n\n{str(adapt_expr)}'
+            M_strs += '\n\n' + str(adapt_expr)
         out_file.write(M_strs)
     else:
         print('')
